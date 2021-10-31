ab06fdd2e563ef0e921bc04bce0f388b1a33694e
Wigandt de Klerk <wigandt@ualberta.ca>
2021-10-31 01:11:56 -0600

    Created EventListAdapter class to handles card construction for EventsFragment as HabitListAdapter does for Habit. (Filled with errors for now)

---
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/model/EventListAdapter.java
@@ -0,0 +1,114 @@
+package ca.cmput301f21t22.nabu.model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+import ca.cmput301f21t22.nabu.R;
+import ca.cmput301f21t22.nabu.databinding.HabitCardBinding;
+
+public class EventListAdapter extends ArrayAdapter<Event> {
+
+    private ArrayList<Event> events;
+    private Context context;
+    @Nullable
+    private HabitCardBinding binding;
+    @Nullable
+    ViewGroup container;
+
+    public EventListAdapter(Context context, ArrayList<Event> events) {
+        super(context, 0, events);
+
+        this.events = events;
+        this.context = context;
+    }
+
+    //Set up views for list objects
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        View view = convertView;
+
+        if(view == null){
+            this.binding = EventCardBinding.inflate(LayoutInflater.from(context));
+        }
+
+        Event event = events.get(position);
+
+        TextView eventComment = this.binding.eventCommentText;
+        TextView dates = this.binding.eventdatesText;
+        TextView eventPhoto = this.binding.eventPhoto;
+        TextView eventLocation  = this.binding.eventLocation;
+
+        eventComment.setText(event.getComment());
+        dates.setText(event.getDate().toString());
+        eventPhoto.setText(event.getPhotoPath());
+        eventLocation.setText(event.getLocation());
+
+        final ImageButton eventsMenuButton = this.binding.eventsPopupImageButton;
+        eventssMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu eventsPopupMenu = new PopupMenu(context, eventsMenuButton);
+                MenuInflater inflater = eventsPopupMenu.getMenuInflater();
+                inflater.inflate(R.menu.habit_card_popup_menu, eventsPopupMenu.getMenu());
+                eventsPopupMenu.show();
+                final PopupMenu menu = eventsPopupMenu;
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if (item.getItemId() == eventsPopupMenu.getMenu().getItem(0).getItemId()) {
+                            //Edit
+                            Event inputEvent = new Event();
+                            //TODO: Add call for edit habit fragment
+                            edit(position, inputEvent);
+                            return true;
+                        }
+                        else if (item.getItemId() == eventsPopupMenu.getMenu().getItem(1).getItemId()) {
+                            //remove
+                            remove(events.get(position));
+                            return true;
+                        }
+                        else { return false; } //Default
+                    }
+                });
+            }
+        });
+
+        return this.binding.getRoot();
+    }
+
+    @Override
+    public void add(@Nullable Event event) {
+        super.add(event);
+        //this.habits.add(habit);
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public void remove(@Nullable Event event) {
+        super.remove(event);
+        //this.habits.remove(habit);
+        this.notifyDataSetChanged();
+    }
+
+    public void edit(int position, @Nullable Event event) {
+        this.remove(this.getItem(position));
+        this.insert(event, position);
+        //this.habits.set(position, habit);
+        this.notifyDataSetChanged();
+    }
+}
