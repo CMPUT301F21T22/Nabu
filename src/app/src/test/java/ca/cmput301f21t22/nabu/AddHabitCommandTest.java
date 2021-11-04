9eaaef664fe1a2d2a6d422f1ad568596d36fc798
Wigandt de Klerk <wigandt@ualberta.ca>
2021-11-04 17:41:11 -0600

    Updated bases and created the beginning of tests for some of the commands

--- src/app/src/test/java/ca/cmput301f21t22/nabu/AddHabitCommandTest.java
+++ src/app/src/test/java/ca/cmput301f21t22/nabu/AddHabitCommandTest.java
@@ -1,4 +1,41 @@
 package ca.cmput301f21t22.nabu;
 
+import static org.junit.Assert.assertEquals;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.GeoPoint;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+import ca.cmput301f21t22.nabu.data.Event;
+import ca.cmput301f21t22.nabu.data.Habit;
+import ca.cmput301f21t22.nabu.data.Occurrence;
+import ca.cmput301f21t22.nabu.model.commands.AddEventCommand;
+
 public class AddHabitCommandTest {
+    @Before
+    @Test
+    private void fireSetUp() {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.useEmulator("10.0.2.2", 8080);
+
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(false)
+                .build();
+        firestore.setFirestoreSettings(settings);
+    }
+
+    @Test
+    private void testAddHabitCommand() {
+        Habit habit = new Habit("Floss teeth", "get healthier teeth", new GregorianCalendar().getTime(), new Occurrence(), new ArrayList<>(), false);
+
+        AddEventCommand addHabitCommand = new AddEventCommand(habit, new Event());
+        assertEquals(habit, addHabitCommand.execute());
+        //TODO: add process for checking database
+    }
 }
--- src/app/src/test/java/ca/cmput301f21t22/nabu/DeleteEventCommandTest.java
+++ src/app/src/test/java/ca/cmput301f21t22/nabu/DeleteEventCommandTest.java
@@ -1,4 +1,40 @@
 package ca.cmput301f21t22.nabu;
 
+import static org.junit.Assert.assertEquals;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.GeoPoint;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+
+import ca.cmput301f21t22.nabu.data.Event;
+import ca.cmput301f21t22.nabu.data.Habit;
+import ca.cmput301f21t22.nabu.model.commands.AddEventCommand;
+import ca.cmput301f21t22.nabu.model.commands.DeleteEventCommand;
+
 public class DeleteEventCommandTest {
+    @Before
+    @Test
+    private void fireSetUp() {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.useEmulator("10.0.2.2", 8080);
+
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(false)
+                .build();
+        firestore.setFirestoreSettings(settings);
+    }
+
+    private void testDeleteEventCommand() {
+        Event event = new Event((new GregorianCalendar()).getTime(), "another day another floss", "user/events/event300", new GeoPoint(15, 78));
+
+        AddEventCommand addEventCommand = new AddEventCommand(new Habit(), event);
+        DeleteEventCommand deleteEventCommand = new DeleteEventCommand(event);
+        assertEquals(true, deleteEventCommand.execute());
+        //TODO: add process for checking database
+    }
 }
