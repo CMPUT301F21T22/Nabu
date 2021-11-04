d725831e21ddd1a19f6bfa6f0d38b4c457fee85a
Wigandt de Klerk <wigandt@ualberta.ca>
2021-11-04 16:12:18 -0600

    Created bases for command tests

---
+++ src/app/src/test/java/ca/cmput301f21t22/nabu/DeleteEventCommandTest.java
@@ -0,0 +1,4 @@
+package ca.cmput301f21t22.nabu;
+
+public class DeleteEventCommandTest {
+}
---
+++ src/app/src/test/java/ca/cmput301f21t22/nabu/MyDayCardTest.java
@@ -0,0 +1,17 @@
+package ca.cmput301f21t22.nabu;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import ca.cmput301f21t22.nabu.data.Event;
+import ca.cmput301f21t22.nabu.data.Habit;
+import ca.cmput301f21t22.nabu.data.MyDayCard;
+import ca.cmput301f21t22.nabu.data.Occurrence;
+
+public class MyDayCardTest {
+    private Habit habit = new Habit("104","Feed Cat", "They are hungry",
+            new Date(2020, 11, 12), new Occurrence(false,
+            false, false, false, false, false,
+            false), new ArrayList<String>(), true);
+    //private Event[] events = new Event[]
+}
