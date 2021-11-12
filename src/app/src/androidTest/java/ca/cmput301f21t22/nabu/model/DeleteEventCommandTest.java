18414c814a6d8b3961dae79c176c6d62c938c74b
Teng Liu <teng@trnglina.org>
2021-11-12 13:55:06 -0700

    Foundational changes for model testing

---
+++ src/app/src/androidTest/java/ca/cmput301f21t22/nabu/model/DeleteEventCommandTest.java
@@ -0,0 +1,3 @@
+package ca.cmput301f21t22.nabu.model;
+
+public class DeleteEventCommandTest extends FirestoreTest {}
---
+++ src/app/src/androidTest/java/ca/cmput301f21t22/nabu/model/UpdateHabitCommandTest.java
@@ -0,0 +1,3 @@
+package ca.cmput301f21t22.nabu.model;
+
+public class UpdateHabitCommandTest extends FirestoreTest {}
