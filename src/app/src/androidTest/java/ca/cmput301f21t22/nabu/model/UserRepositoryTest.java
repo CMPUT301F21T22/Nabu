cb098ca418ec76c22a7576529d9909965de7b3b6
Teng Liu <teng@trnglina.org>
2021-11-14 08:51:29 -0700

    Tests for UserRepository

--- src/app/src/androidTest/java/ca/cmput301f21t22/nabu/model/UserRepositoryTest.java
+++ src/app/src/androidTest/java/ca/cmput301f21t22/nabu/model/UserRepositoryTest.java
@@ -2,23 +2,29 @@ package ca.cmput301f21t22.nabu.model;
 
 import static org.awaitility.Awaitility.await;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.Observer;
 
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FieldValue;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import ca.cmput301f21t22.nabu.data.User;
 import ca.cmput301f21t22.nabu.model.repositories.UserRepository;
@@ -58,19 +64,131 @@ public class UserRepositoryTest extends AuthenticatedFirestoreTest {
     }
 
     @Test
-    public void liveNewAccountSignIn() {
+    public void liveNewAccountSignIn() throws ExecutionException, InterruptedException {
+        AtomicBoolean usersComplete = new AtomicBoolean(false);
+        Observer<Map<String, User>> usersObserver = users -> {
+            if (users.size() >= 1) {
+                Optional<User> query = users.values().stream().findAny();
+                assertTrue(query.isPresent());
+                User user = query.get();
+                assertNotEquals("", user.getId());
+                assertEquals("test3@example.com", user.getEmail());
+                assertEquals(new ArrayList<>(), user.getHabits());
+
+                usersComplete.set(true);
+            }
+        };
+
+        AtomicBoolean currentUserComplete = new AtomicBoolean(false);
+        Observer<User> currentUserObserver = user -> {
+            if (user != null) {
+                assertNotEquals("", user.getId());
+                assertEquals("test3@example.com", user.getEmail());
+                assertEquals(new ArrayList<>(), user.getHabits());
+
+                currentUserComplete.set(true);
+            }
+        };
+
+        this.repository.getUsers().observeForever(usersObserver);
+        this.repository.getCurrentUser().observeForever(currentUserObserver);
+
+        this.createUser("test3@example.com", "password3");
+
+        await().until(usersComplete::get);
+        await().until(currentUserComplete::get);
+        this.repository.getUsers().removeObserver(usersObserver);
+        this.repository.getCurrentUser().removeObserver(currentUserObserver);
     }
 
     @Test
-    public void liveExistingAccountSignIn() {
+    public void liveExistingAccountSignIn() throws ExecutionException, InterruptedException {
+        this.createUser("test3@example.com", "password3");
+        await().until(() -> this.repository.getCurrentUser().getValue() != null);
+        this.auth.signOut();
+        await().until(() -> this.repository.getCurrentUser().getValue() == null);
+
+        AtomicBoolean usersComplete = new AtomicBoolean(false);
+        Observer<Map<String, User>> usersObserver = users -> {
+            if (users.size() >= 1) {
+                Optional<User> query = users.values().stream().findAny();
+                assertTrue(query.isPresent());
+                User user = query.get();
+                assertNotEquals("", user.getId());
+                assertEquals("test3@example.com", user.getEmail());
+                assertEquals(new ArrayList<>(), user.getHabits());
+
+                usersComplete.set(true);
+            }
+        };
+
+        AtomicBoolean currentUserComplete = new AtomicBoolean(false);
+        Observer<User> currentUserObserver = user -> {
+            if (user != null) {
+                assertNotEquals("", user.getId());
+                assertEquals("test3@example.com", user.getEmail());
+                assertEquals(new ArrayList<>(), user.getHabits());
+
+                currentUserComplete.set(true);
+            }
+        };
+
+        this.repository.getUsers().observeForever(usersObserver);
+        this.repository.getCurrentUser().observeForever(currentUserObserver);
+
+        this.auth.signInWithEmailAndPassword("test3@example.com", "password3");
+
+        await().until(usersComplete::get);
+        await().until(currentUserComplete::get);
+        this.repository.getUsers().removeObserver(usersObserver);
+        this.repository.getCurrentUser().removeObserver(currentUserObserver);
     }
 
     @Test
-    public void liveSignOut() {
-    }
+    public void liveUserUpdates() throws ExecutionException, InterruptedException {
+        this.createUser("test3@example.com", "password3");
+        await().until(() -> this.repository.getCurrentUser().getValue() != null);
 
-    @Test
-    public void liveUserUpdates() {
+        AtomicBoolean usersComplete = new AtomicBoolean(false);
+        AtomicBoolean usersReceived = new AtomicBoolean(false);
+        Observer<Map<String, User>> usersObserver = users -> {
+            if (users.size() >= 1 && !usersReceived.get()) {
+                usersReceived.set(true);
+            } else if (users.size() >= 1 && usersReceived.get()) {
+                Optional<User> query = users.values().stream().findAny();
+                assertTrue(query.isPresent());
+                User user = query.get();
+
+                assertEquals(Collections.singletonList("iTqYxu1N6IO72GNHFBtX"), user.getHabits());
+
+                usersComplete.set(true);
+            }
+        };
+
+        AtomicBoolean currentUserComplete = new AtomicBoolean(false);
+        AtomicBoolean currentUserReceived = new AtomicBoolean(false);
+        Observer<User> currentUserObserver = user -> {
+            if (user != null && !currentUserReceived.get()) {
+                currentUserReceived.set(true);
+            } else if (user != null && currentUserReceived.get()) {
+                assertEquals(Collections.singletonList("iTqYxu1N6IO72GNHFBtX"), user.getHabits());
+
+                currentUserComplete.set(true);
+            }
+        };
+
+        this.repository.getUsers().observeForever(usersObserver);
+        this.repository.getCurrentUser().observeForever(currentUserObserver);
+
+        FirebaseUser user = this.auth.getCurrentUser();
+        assertNotNull(user);
+
+        this.collection.document(user.getUid()).update("habits", FieldValue.arrayUnion("iTqYxu1N6IO72GNHFBtX"));
+
+        await().until(usersComplete::get);
+        await().until(currentUserComplete::get);
+        this.repository.getUsers().removeObserver(usersObserver);
+        this.repository.getCurrentUser().removeObserver(currentUserObserver);
     }
 
     private void createUser(String email, String password) throws ExecutionException, InterruptedException {
