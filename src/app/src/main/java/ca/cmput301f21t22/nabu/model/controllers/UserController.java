a08620807433d16cc109d6d7c93bde0471d6f11d
Teng Liu <teng@trnglina.org>
2021-11-20 15:36:50 -0700

    Tentative social implementation for UserController

--- src/app/src/main/java/ca/cmput301f21t22/nabu/model/controllers/UserController.java
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/model/controllers/UserController.java
@@ -166,6 +166,154 @@ public class UserController {
         return future;
     }
 
+    /**
+     * Add a user reference to the list of users a user is following.
+     *
+     * @param userId         The ID of the user to update.
+     * @param followTargetId The ID of the user being followed.
+     * @return A future for the ID of the user, once updated.
+     */
+    public CompletableFuture<String> addFollowing(@NonNull String userId, @NonNull String followTargetId) {
+        if (userId.equals("") || followTargetId.equals("")) {
+            throw new IllegalArgumentException();
+        }
+
+        CompletableFuture<String> future = new CompletableFuture<>();
+        this.usersCollection.document(userId)
+                .update("following", FieldValue.arrayUnion(followTargetId))
+                .addOnSuccessListener(unused -> {
+                    future.complete(userId);
+                    Log.d(TAG, "Follow target: " + followTargetId + " added to user: " + userId);
+                })
+                .addOnFailureListener(unused -> {
+                    future.complete(userId);
+                    Log.d(TAG, "Failed to add follow target: " + followTargetId + " to user: " + userId);
+                });
+        return future;
+    }
+
+    /**
+     * Delete a user reference from the list of users a user is following.
+     *
+     * @param userId         The ID of the user to update.
+     * @param followTargetId The ID of the user to unfollow.
+     * @return A future for the ID of the user, once updated.
+     */
+    public CompletableFuture<String> deleteFollowing(@NonNull String userId, @NonNull String followTargetId) {
+        if (userId.equals("") || followTargetId.equals("")) {
+            throw new IllegalArgumentException();
+        }
+
+        CompletableFuture<String> future = new CompletableFuture<>();
+        this.usersCollection.document(userId)
+                .update("following", FieldValue.arrayRemove(followTargetId))
+                .addOnSuccessListener(unused -> {
+                    future.complete(userId);
+                    Log.d(TAG, "Follow target: " + followTargetId + " removed from user: " + userId);
+                })
+                .addOnFailureListener(unused -> {
+                    future.complete(userId);
+                    Log.d(TAG, "Failed to remove follow target: " + followTargetId + " from user: " + userId);
+                });
+        return future;
+    }
+
+    /**
+     * Remove all references a user is following
+     *
+     * @param userId The ID of the user to update.
+     * @return A future for the ID of the user, once updated.
+     */
+    public CompletableFuture<String> clearFollowing(@NonNull String userId) {
+        if (userId.equals("")) {
+            throw new IllegalArgumentException();
+        }
+
+        CompletableFuture<String> future = new CompletableFuture<>();
+        this.usersCollection.document(userId).update("following", new ArrayList<>()).addOnSuccessListener(unused -> {
+            future.complete(userId);
+            Log.d(TAG, "Cleared follow targets from user: " + userId);
+        }).addOnFailureListener(unused -> {
+            future.complete(userId);
+            Log.d(TAG, "Failed to clear follow targets from user: " + userId);
+        });
+        return future;
+    }
+
+    /**
+     * Add a request to the list of incoming follow requests of a user.
+     *
+     * @param userId      The ID of the user to update.
+     * @param requesterId The ID of the user from whom the request comes.
+     * @return A future for the ID of the user, once updated.
+     */
+    public CompletableFuture<String> addRequests(@NonNull String userId, @NonNull String requesterId) {
+        if (userId.equals("") || requesterId.equals("")) {
+            throw new IllegalArgumentException();
+        }
+
+        CompletableFuture<String> future = new CompletableFuture<>();
+        this.usersCollection.document(userId)
+                .update("requests", FieldValue.arrayUnion(requesterId))
+                .addOnSuccessListener(unused -> {
+                    future.complete(userId);
+                    Log.d(TAG, "Request from: " + requesterId + " added to user: " + userId);
+                })
+                .addOnFailureListener(unused -> {
+                    future.complete(userId);
+                    Log.d(TAG, "Failed to add request from: " + requesterId + " to user: " + userId);
+                });
+        return future;
+    }
+
+    /**
+     * Remove a request from the list of incoming follow requests of a user.
+     *
+     * @param userId      The ID of the user to update.
+     * @param requesterId The ID of the user whose request should be deleted.
+     * @return A future for the ID of the user, once updated.
+     */
+    public CompletableFuture<String> deleteRequests(@NonNull String userId, @NonNull String requesterId) {
+        if (userId.equals("") || requesterId.equals("")) {
+            throw new IllegalArgumentException();
+        }
+
+        CompletableFuture<String> future = new CompletableFuture<>();
+        this.usersCollection.document(userId)
+                .update("requests", FieldValue.arrayRemove(requesterId))
+                .addOnSuccessListener(unused -> {
+                    future.complete(userId);
+                    Log.d(TAG, "Request from: " + requesterId + " removed from user: " + userId);
+                })
+                .addOnFailureListener(unused -> {
+                    future.complete(userId);
+                    Log.d(TAG, "Failed to remove request from: " + requesterId + " from user: " + userId);
+                });
+        return future;
+    }
+
+    /**
+     * Remove all incoming follow requests of a user.
+     *
+     * @param userId The ID of the user to update.
+     * @return A future for the ID of the user, once updated.
+     */
+    public CompletableFuture<String> clearRequests(@NonNull String userId) {
+        if (userId.equals("")) {
+            throw new IllegalArgumentException();
+        }
+
+        CompletableFuture<String> future = new CompletableFuture<>();
+        this.usersCollection.document(userId).update("requests", new ArrayList<>()).addOnSuccessListener(unused -> {
+            future.complete(userId);
+            Log.d(TAG, "Requests cleared to user: " + userId);
+        }).addOnFailureListener(unused -> {
+            future.complete(userId);
+            Log.d(TAG, "Failed to clear requests to user: " + userId);
+        });
+        return future;
+    }
+
     private void onSignInChanged(FirebaseAuth auth) {
         FirebaseUser user = auth.getCurrentUser();
         if (user == null) {
