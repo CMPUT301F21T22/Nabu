0f0647f72d0a4c91bc9c6e8df6375c14a0943c34
Wigandt de Klerk <wigandt@ualberta.ca>
2021-11-20 02:05:10 -0700

    Finished major changes and edits to the adapter for following request cards

---
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/ui/social/RequestCardAdapter.java
@@ -0,0 +1,90 @@
+package ca.cmput301f21t22.nabu.ui.social;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.cmput301f21t22.nabu.data.User;
+import ca.cmput301f21t22.nabu.databinding.CardUserRequestBinding;
+import ca.cmput301f21t22.nabu.ui.ItemClickListener;
+
+public class RequestCardAdapter extends RecyclerView.Adapter<RequestCardAdapter.ViewHolder> {
+    @NonNull
+    private List<User> requests;
+    @Nullable
+    private ItemClickListener<RequestCardAdapter, User> requestCardRejectButtonListener;
+    @Nullable
+    private ItemClickListener<RequestCardAdapter, User> requestCardAcceptButtonListener;
+
+    public RequestCardAdapter() {
+        this.requests = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(
+                CardUserRequestBinding.inflate(LayoutInflater.from(parent.getContext()),
+                        parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = this.requests.get(position);
+
+        holder.onBindView(user);
+        holder.binding.rejectFollowButton.setOnClickListener((view) -> {
+            if (this.requestCardRejectButtonListener != null) {
+                this.requestCardRejectButtonListener.onItemClicked(this, user);
+            }
+        });
+        holder.binding.acceptFollowButton.setOnClickListener((view) -> {
+            if (this.requestCardAcceptButtonListener != null) {
+                this.requestCardAcceptButtonListener.onItemClicked(this, user);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.requests.size();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void setCards(@Nullable List<User> requests) {
+        requests = requests != null ? requests : new ArrayList<>();
+        this.requests = requests;
+        this.notifyDataSetChanged();
+    }
+
+    public void setDenyButtonListener(@Nullable ItemClickListener<RequestCardAdapter, User>
+                                              requestCardDenyButtonListener) {
+        this.requestCardRejectButtonListener = requestCardDenyButtonListener;
+    }
+
+    public void setAcceptButtonListener(@Nullable ItemClickListener<RequestCardAdapter, User>
+                                                requestCardAcceptButtonListener) {
+        this.requestCardAcceptButtonListener = requestCardAcceptButtonListener;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        @NonNull
+        private final CardUserRequestBinding binding;
+
+        public ViewHolder(@NonNull CardUserRequestBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void onBindView(@NonNull User user) {
+            this.binding.labelUserName.setText(user.getEmail());
+        }
+    }
+}
