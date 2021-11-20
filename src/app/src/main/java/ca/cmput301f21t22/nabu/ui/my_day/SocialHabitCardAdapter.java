b6fd31051535fc15ae8ff152ed6f7e98c648892b
Wigandt de Klerk <wigandt@ualberta.ca>
2021-11-20 15:27:28 -0700

    Added documentation

--- src/app/src/main/java/ca/cmput301f21t22/nabu/ui/my_day/SocialHabitCardAdapter.java
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/ui/my_day/SocialHabitCardAdapter.java
@@ -18,32 +18,66 @@ import android.widget.ImageView;
 
 import ca.cmput301f21t22.nabu.data.MyDayCard;
 
-public class SocialHabitCardAdapter extends RecyclerView.Adapter<SocialHabitCardAdapter.ViewHolder> {
+/**
+ * Creates UI cards that represent another user's habits by adapting data from MyDayCards.
+ */
+public class SocialHabitCardAdapter extends
+        RecyclerView.Adapter<SocialHabitCardAdapter.ViewHolder> {
     @NonNull
     private List<MyDayCard> cards;
 
+    /**
+     * Assigns a new array list for the list of habit cards for social feeds.
+     */
     public SocialHabitCardAdapter() {
         this.cards = new ArrayList<>();
     }
 
+    /**
+     * Sets up the card view on the parent view group.
+     * @param parent
+     *  The parent ViewGroup, the view for this to be placed within.
+     * @param viewType
+     *  An integer representation of the type of view of the parent ViewGroup.
+     * @return
+     *  The ViewHolder, an object representing the card view created.
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ViewHolder(
-                CardSocialHabitBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+                CardSocialHabitBinding.inflate(LayoutInflater.from(parent.getContext()), parent,
+                        false));
     }
 
+    /**
+     * Retrieves information for creating the card and then initiates the card's creation.
+     * @param holder
+     *  A ViewHolder that represents the card view being created.
+     * @param position
+     *  The integer position, within the cards list, of the HabitCard to take information from.
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         MyDayCard card = this.cards.get(position);
         holder.onBindView(card);
     }
 
+    /**
+     * Returns an integer count on how many habits are going to be or have been adapted for a feed.
+     * @return
+     *  The integer amount of the habits that should be adapted.
+     */
     @Override
     public int getItemCount() {
         return this.cards.size();
     }
 
+    /**
+     * Sets a list of MyDayCards to be adapted.
+     * @param cards
+     *  A MyDayCard List of MyDayCards to be adapted.
+     */
     @SuppressLint("NotifyDataSetChanged")
     public void setCards(@Nullable List<MyDayCard> cards) {
         cards = cards != null ? cards : new ArrayList<>();
@@ -51,15 +85,28 @@ public class SocialHabitCardAdapter extends RecyclerView.Adapter<SocialHabitCard
         this.notifyDataSetChanged();
     }
 
+    /**
+     * Manages the view of the card.
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         @NonNull
         private final CardSocialHabitBinding binding;
 
+        /**
+         * Sets the binding of the view to a layout binding instance.
+         * @param binding
+         *  The layout binding instance to be set.
+         */
         public ViewHolder(@NonNull CardSocialHabitBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
 
+        /**
+         * Makes the proper edits to display the details of a MyDayCard within the view.
+         * @param card
+         *  The MyDayCard to take data from to display.
+         */
         public void onBindView(@NonNull MyDayCard card) {
             this.binding.labelHabitName.setPaintFlags(
                     this.binding.labelHabitName.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
--- src/app/src/main/java/ca/cmput301f21t22/nabu/ui/social/RequestFollowCardAdapter.java
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/ui/social/RequestFollowCardAdapter.java
@@ -15,7 +15,12 @@ import ca.cmput301f21t22.nabu.data.User;
 import ca.cmput301f21t22.nabu.databinding.CardUserRequestBinding;
 import ca.cmput301f21t22.nabu.ui.ItemClickListener;
 
-public class RequestFollowCardAdapter extends RecyclerView.Adapter<RequestFollowCardAdapter.ViewHolder> {
+/**
+ * Creates UI cards for representing another user's request to follow the user. Detects input on
+ * those cards on accepting or rejecting the follow request.
+ */
+public class RequestFollowCardAdapter extends
+        RecyclerView.Adapter<RequestFollowCardAdapter.ViewHolder> {
     @NonNull
     private List<User> requests;
     @Nullable
@@ -23,10 +28,22 @@ public class RequestFollowCardAdapter extends RecyclerView.Adapter<RequestFollow
     @Nullable
     private ItemClickListener<RequestFollowCardAdapter, User> requestCardAcceptButtonListener;
 
+    /**
+     * Assigns a new array list for the list of other users that are requesting to follow the user.
+     */
     public RequestFollowCardAdapter() {
         this.requests = new ArrayList<>();
     }
 
+    /**
+     * Sets up the card view on the parent view group.
+     * @param parent
+     *  The parent ViewGroup, the view for this to be placed within.
+     * @param viewType
+     *  An integer representation of the type of view of the parent ViewGroup.
+     * @return
+     *  The ViewHolder, an object representing the card view created.
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -35,6 +52,13 @@ public class RequestFollowCardAdapter extends RecyclerView.Adapter<RequestFollow
                         parent, false));
     }
 
+    /**
+     * Retrieves information for creating the card and then initiates the card's creation.
+     * @param holder
+     *  A ViewHolder that represents the card view being created.
+     * @param position
+     *  The integer position, within the requests list, of the User to take information from.
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         User user = this.requests.get(position);
@@ -52,11 +76,21 @@ public class RequestFollowCardAdapter extends RecyclerView.Adapter<RequestFollow
         });
     }
 
+    /**
+     * Returns an integer count on how many Users are going to be or have been adapted.
+     * @return
+     *  The integer amount of the Users that should be adapted.
+     */
     @Override
     public int getItemCount() {
         return this.requests.size();
     }
 
+    /**
+     * Sets a list of Users to be adapted.
+     * @param requests
+     *  A User List of Users to be adapted.
+     */
     @SuppressLint("NotifyDataSetChanged")
     public void setCards(@Nullable List<User> requests) {
         requests = requests != null ? requests : new ArrayList<>();
@@ -64,25 +98,48 @@ public class RequestFollowCardAdapter extends RecyclerView.Adapter<RequestFollow
         this.notifyDataSetChanged();
     }
 
+    /**
+     * Sets the listener for the deny follow request button of the card.
+     * @param requestCardDenyButtonListener
+     *  The listener to be set.
+     */
     public void setDenyButtonListener(@Nullable ItemClickListener<RequestFollowCardAdapter, User>
                                               requestCardDenyButtonListener) {
         this.requestCardRejectButtonListener = requestCardDenyButtonListener;
     }
 
+    /**
+     * Sets the listener for the accept follow request button of the card.
+     * @param requestCardAcceptButtonListener
+     *  The listener to be set.
+     */
     public void setAcceptButtonListener(@Nullable ItemClickListener<RequestFollowCardAdapter, User>
                                                 requestCardAcceptButtonListener) {
         this.requestCardAcceptButtonListener = requestCardAcceptButtonListener;
     }
 
+    /**
+     * Manages the view of the card.
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         @NonNull
         private final CardUserRequestBinding binding;
 
+        /**
+         * Sets the binding of the view to a layout binding instance.
+         * @param binding
+         *  The layout binding instance to be set.
+         */
         public ViewHolder(@NonNull CardUserRequestBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
 
+        /**
+         * Makes the proper edits to display the user's username within the view.
+         * @param user
+         *  The user to take data from to display.
+         */
         public void onBindView(@NonNull User user) {
             this.binding.labelUserName.setText(user.getEmail());
         }
