33ce20f5cb50287436b436de04f280cfe3f16d99
Teng Liu <teng@trnglina.org>
2021-11-22 10:39:30 -0700

    Documentation for new commands

--- src/app/src/main/java/ca/cmput301f21t22/nabu/model/commands/AcceptRequestCommand.java
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/model/commands/AcceptRequestCommand.java
@@ -8,6 +8,9 @@ import ca.cmput301f21t22.nabu.data.User;
 import ca.cmput301f21t22.nabu.model.Command;
 import ca.cmput301f21t22.nabu.model.controllers.UserController;
 
+/**
+ * Used to call on controllers to accept an incoming follow request.
+ */
 public class AcceptRequestCommand implements Command<CompletableFuture<Boolean>> {
     @NonNull
     private final UserController userController;
@@ -17,6 +20,12 @@ public class AcceptRequestCommand implements Command<CompletableFuture<Boolean>>
     @NonNull
     private final User sender;
 
+    /**
+     * Create an instance of AcceptRequestCommand.
+     *
+     * @param receiver The user that has received a follow request. This must be a remote-linked instance.
+     * @param sender   The user that sent the follow request. This must be a remote-linked instance.
+     */
     public AcceptRequestCommand(@NonNull User receiver, @NonNull User sender) {
         this.userController = UserController.getInstance();
 
@@ -24,6 +33,11 @@ public class AcceptRequestCommand implements Command<CompletableFuture<Boolean>>
         this.sender = sender;
     }
 
+    /**
+     * Execute the command.
+     *
+     * @return A future indicating whether the operation succeeded.
+     */
     @Override
     public CompletableFuture<Boolean> execute() {
         return this.userController.deleteRequest(this.receiver.getId(), this.sender.getId())
--- src/app/src/main/java/ca/cmput301f21t22/nabu/model/commands/SendRequestCommand.java
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/model/commands/SendRequestCommand.java
@@ -8,6 +8,9 @@ import ca.cmput301f21t22.nabu.data.User;
 import ca.cmput301f21t22.nabu.model.Command;
 import ca.cmput301f21t22.nabu.model.controllers.UserController;
 
+/**
+ * Used to call on controllers to send a follow request to a user.
+ */
 public class SendRequestCommand implements Command<CompletableFuture<Boolean>> {
     @NonNull
     private final UserController userController;
@@ -17,6 +20,12 @@ public class SendRequestCommand implements Command<CompletableFuture<Boolean>> {
     @NonNull
     private final User toUser;
 
+    /**
+     * Create an instance of SendRequestCommand.
+     *
+     * @param fromUser The user that sends the follow request. This must be a remote-linked instance.
+     * @param toUser   The user that receives the follow request. This must be a remote-linked instance.
+     */
     public SendRequestCommand(@NonNull User fromUser, @NonNull User toUser) {
         this.userController = UserController.getInstance();
 
@@ -24,6 +33,11 @@ public class SendRequestCommand implements Command<CompletableFuture<Boolean>> {
         this.toUser = toUser;
     }
 
+    /**
+     * Execute the command.
+     *
+     * @return A future indicating whether the operation succeeded.
+     */
     @Override
     public CompletableFuture<Boolean> execute() {
         return this.userController.addRequest(this.toUser.getId(), this.fromUser.getId())
