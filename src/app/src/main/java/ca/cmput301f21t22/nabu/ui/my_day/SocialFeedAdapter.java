06d6ee0bcec506e37e7d86ac7adea8ea93962b78
Wigandt de Klerk <wigandt@ualberta.ca>
2021-11-22 16:42:50 -0700

    Completed SocialFeedAdapter and made adjustments for changes in data processing.

--- src/app/src/main/java/ca/cmput301f21t22/nabu/ui/my_day/SocialFeedAdapter.java
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/ui/my_day/SocialFeedAdapter.java
@@ -19,6 +19,7 @@ import java.util.Map;
 import ca.cmput301f21t22.nabu.data.Event;
 import ca.cmput301f21t22.nabu.data.Habit;
 import ca.cmput301f21t22.nabu.data.HabitCard;
+import ca.cmput301f21t22.nabu.data.MyDayUserCard;
 import ca.cmput301f21t22.nabu.data.User;
 import ca.cmput301f21t22.nabu.databinding.CardHabitBinding;
 import ca.cmput301f21t22.nabu.databinding.CardSocialHabitBinding;
@@ -29,7 +30,7 @@ import ca.cmput301f21t22.nabu.ui.habits.EventCardAdapter;
 
 public class SocialFeedAdapter extends RecyclerView.Adapter<SocialFeedAdapter.ViewHolder> {
     @NonNull
-    private List<User> users;
+    private List<MyDayUserCard> users;
 
     public SocialFeedAdapter() {
         this.users = new ArrayList<>();
@@ -45,10 +46,10 @@ public class SocialFeedAdapter extends RecyclerView.Adapter<SocialFeedAdapter.Vi
 
     @Override
     public void onBindViewHolder(@NonNull SocialFeedAdapter.ViewHolder holder, int position) {
-        User user = this.users.get(position);
-        List<Habit> habits = habits;
-        holder.adapter.setCards();
-        holder.onBindView(user);
+        MyDayUserCard userCard = this.users.get(position);
+
+        holder.adapter.setCards(userCard.getUserHabits());
+        holder.onBindView(userCard);
     }
 
     @Override
@@ -57,7 +58,7 @@ public class SocialFeedAdapter extends RecyclerView.Adapter<SocialFeedAdapter.Vi
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    public void setCards(@Nullable List<User> users) {
+    public void setCards(@Nullable List<MyDayUserCard> users) {
         users = users != null ? users : new ArrayList<>();
         this.users = users;
         this.notifyDataSetChanged();
@@ -75,125 +76,10 @@ public class SocialFeedAdapter extends RecyclerView.Adapter<SocialFeedAdapter.Vi
             this.adapter = adapter;
         }
 
-        public void onBindView(@NonNull User user) {
-            this.binding.feedNameText.setText(user.getEmail());
+        public void onBindView(@NonNull MyDayUserCard userCard) {
+            this.binding.feedNameText.setText(userCard.getEmail());
+
+            this.binding.feedView.setAdapter(this.adapter);
         }
     }
 }
-
-/*
-public class HabitsFragment extends ExtendedToolbarFragment {
-    @NonNull
-    public final static String TAG = "HabitsFragment";
-
-    @Nullable
-    private HabitsViewModel viewModel;
-    @Nullable
-    private FragmentHabitsBinding binding;
-    @Nullable
-    private ca.cmput301f21t22.nabu.ui.habits.HabitCardAdapter adapter;
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        this.viewModel = new ViewModelProvider(this).get(HabitsViewModel.class);
-        this.binding = FragmentHabitsBinding.inflate(inflater, container, false);
-        this.adapter = new ca.cmput301f21t22.nabu.ui.habits.HabitCardAdapter();
-
-        UserRepository.getInstance()
-                .getCurrentUser()
-                .observe(this.getViewLifecycleOwner(), user -> this.viewModel.setCurrentUser(user));
-        HabitRepository.getInstance()
-                .getHabits()
-                .observe(this.getViewLifecycleOwner(), habits -> this.viewModel.setCurrentHabits(habits));
-        EventRepository.getInstance()
-                .getEvents()
-                .observe(this.getViewLifecycleOwner(), events -> this.viewModel.setCurrentEvents(events));
-
-        this.adapter.setHabitCardClickListener((adapter, card) -> this.viewModel.onCardClicked(card));
-        this.adapter.setHabitCardMenuClickListener((view, card) -> {
-            PopupMenu menu = new PopupMenu(this.getContext(), view);
-            if (!card.isExpanded()) {
-                menu.getMenuInflater().inflate(R.menu.overflow_menu_habit_card_unexpanded, menu.getMenu());
-            } else {
-                menu.getMenuInflater().inflate(R.menu.overflow_menu_habit_card_expanded, menu.getMenu());
-            }
-            menu.setOnMenuItemClickListener(menuItem -> {
-                if (menuItem.getItemId() == menu.getMenu().getItem(0).getItemId()) {
-                    this.viewModel.onCardClicked(card);
-                    return true;
-                } else if (menuItem.getItemId() == menu.getMenu().getItem(1).getItemId()) {
-                    EditHabitFragment.newInstance(card.getHabit()).show(this.getChildFragmentManager(), "EditHabit");
-                    return true;
-                } else if (menuItem.getItemId() == menu.getMenu().getItem(2).getItemId()) {
-                    new MaterialAlertDialogBuilder(this.requireContext()).setMessage(
-                            R.string.dialog_delete_habit_message)
-                            .setNegativeButton(R.string.button_cancel, (dialogInterface, i) -> {
-                            })
-                            .setPositiveButton(R.string.button_delete,
-                                    (dialogInterface, i) -> this.viewModel.deleteHabit(card.getHabit()))
-                            .show();
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-            menu.show();
-        });
-
-        this.adapter.setEventCardClickListener((adapter, event) -> EditEventFragment.newInstance(event)
-                .show(this.getChildFragmentManager(), "EditEvent"));
-        this.adapter.setEventCardMenuClickListener((view, event) -> {
-            PopupMenu menu = new PopupMenu(this.getContext(), view);
-            menu.getMenuInflater().inflate(R.menu.overflow_menu_event_card, menu.getMenu());
-            menu.setOnMenuItemClickListener(menuItem -> {
-                if (menuItem.getItemId() == menu.getMenu().getItem(0).getItemId()) {
-                    EditEventFragment.newInstance(event).show(this.getChildFragmentManager(), "EditEvent");
-                    return true;
-                } else if (menuItem.getItemId() == menu.getMenu().getItem(1).getItemId()) {
-                    new MaterialAlertDialogBuilder(this.requireContext()).setMessage(
-                            R.string.dialog_delete_event_message)
-                            .setNegativeButton(R.string.button_cancel, (dialogInterface, i) -> {
-                            })
-                            .setPositiveButton(R.string.button_delete,
-                                    (dialogInterface, i) -> this.viewModel.deleteEvent(event))
-                            .show();
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-            menu.show();
-        });
-
-        this.viewModel.getCards().observe(this.getViewLifecycleOwner(), cards -> this.adapter.setCards(cards));
-
-        this.binding.listHabits.setLayoutManager(new LinearLayoutManager(this.requireContext()));
-        this.binding.listHabits.setAdapter(this.adapter);
-
-        this.binding.buttonAddHabit.setOnClickListener(view -> {
-            User currentUser = this.viewModel.getCurrentUser();
-            if (currentUser != null) {
-                EditHabitFragment.newInstance(currentUser).show(this.getChildFragmentManager(), "AddHabit");
-            }
-        });
-
-        return this.binding.getRoot();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
-
-    @NonNull
-    @Override
-    public View getToolbarView() {
-        HeaderDefaultBinding view = HeaderDefaultBinding.inflate(LayoutInflater.from(this.getContext()));
-        view.title.setText(R.string.fragment_habits_name);
-        return view.getRoot();
-    }
-}
-*/
