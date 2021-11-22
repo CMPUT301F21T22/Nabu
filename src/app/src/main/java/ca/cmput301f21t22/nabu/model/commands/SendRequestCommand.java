66f601df9138012b31aa4add316c07c8a2f9713f
Teng Liu <teng@trnglina.org>
2021-11-22 10:27:13 -0700

    Implement SendRequestCommand

--- src/app/src/main/java/ca/cmput301f21t22/nabu/model/commands/SendRequestCommand.java
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/model/commands/SendRequestCommand.java
@@ -1,18 +1,32 @@
 package ca.cmput301f21t22.nabu.model.commands;
 
+import androidx.annotation.NonNull;
+
 import java.util.concurrent.CompletableFuture;
 
 import ca.cmput301f21t22.nabu.data.User;
 import ca.cmput301f21t22.nabu.model.Command;
+import ca.cmput301f21t22.nabu.model.controllers.UserController;
 
 public class SendRequestCommand implements Command<CompletableFuture<Boolean>> {
+    @NonNull
+    private final UserController userController;
 
-    public SendRequestCommand(User fromUser, User toUser) {
+    @NonNull
+    private final User fromUser;
+    @NonNull
+    private final User toUser;
 
+    public SendRequestCommand(@NonNull User fromUser, @NonNull User toUser) {
+        this.userController = UserController.getInstance();
+
+        this.fromUser = fromUser;
+        this.toUser = toUser;
     }
 
     @Override
     public CompletableFuture<Boolean> execute() {
-        return null;
+        return this.userController.addRequest(this.toUser.getId(), this.fromUser.getId())
+                .thenCompose(sender -> CompletableFuture.supplyAsync(() -> true));
     }
 }
