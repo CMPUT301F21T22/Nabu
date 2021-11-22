f7e47ab1079088c540e002443eeb2cd4a60b3463
Teng Liu <teng@trnglina.org>
2021-11-22 10:32:24 -0700

    Implement AcceptRequestCommand

--- src/app/src/main/java/ca/cmput301f21t22/nabu/model/commands/AcceptRequestCommand.java
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/model/commands/AcceptRequestCommand.java
@@ -1,18 +1,33 @@
 package ca.cmput301f21t22.nabu.model.commands;
 
+import androidx.annotation.NonNull;
+
 import java.util.concurrent.CompletableFuture;
 
 import ca.cmput301f21t22.nabu.data.User;
 import ca.cmput301f21t22.nabu.model.Command;
+import ca.cmput301f21t22.nabu.model.controllers.UserController;
 
 public class AcceptRequestCommand implements Command<CompletableFuture<Boolean>> {
+    @NonNull
+    private final UserController userController;
 
-    public AcceptRequestCommand(User receiver, User sender) {
+    @NonNull
+    private final User receiver;
+    @NonNull
+    private final User sender;
 
+    public AcceptRequestCommand(@NonNull User receiver, @NonNull User sender) {
+        this.userController = UserController.getInstance();
+
+        this.receiver = receiver;
+        this.sender = sender;
     }
 
     @Override
     public CompletableFuture<Boolean> execute() {
-        return null;
+        return this.userController.deleteRequest(this.receiver.getId(), this.sender.getId())
+                .thenCompose(receiver -> this.userController.addFollowing(this.sender.getId(), receiver))
+                .thenCompose(sender -> CompletableFuture.supplyAsync(() -> true));
     }
 }
