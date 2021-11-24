45e1dd729fa498bd1efd6f6d84f19e7f1f1e7829
Wigandt de Klerk <wigandt@ualberta.ca>
2021-11-24 16:58:05 -0700

    Added the bulk of code for SocialFragment's set up. Added some of the code for the initialization of the request and following user card views.

--- src/app/src/main/java/ca/cmput301f21t22/nabu/ui/social/SocialFragment.java
+++ src/app/src/main/java/ca/cmput301f21t22/nabu/ui/social/SocialFragment.java
@@ -8,18 +8,25 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import ca.cmput301f21t22.nabu.R;
 import ca.cmput301f21t22.nabu.databinding.FragmentSocialBinding;
 import ca.cmput301f21t22.nabu.databinding.HeaderDefaultBinding;
+import ca.cmput301f21t22.nabu.model.repositories.UserRepository;
 import ca.cmput301f21t22.nabu.ui.ExtendedToolbarFragment;
 
 public class SocialFragment extends ExtendedToolbarFragment {
-
+    @NonNull
+    public final static String TAG = "SocialFragment";
     @Nullable
     private SocialViewModel viewModel;
     @Nullable
     private FragmentSocialBinding binding;
+    @Nullable
+    private RequestFollowCardAdapter requestAdapter;
+    @Nullable
+    private FollowingCardAdapter followingAdapter;
 
     @Nullable
     @Override
@@ -28,6 +35,28 @@ public class SocialFragment extends ExtendedToolbarFragment {
                              @Nullable Bundle savedInstanceState) {
         this.viewModel = new ViewModelProvider(this).get(SocialViewModel.class);
         this.binding = FragmentSocialBinding.inflate(inflater, container, false);
+        this.requestAdapter = new RequestFollowCardAdapter();
+        this.followingAdapter = new FollowingCardAdapter();
+
+        UserRepository.getInstance()
+                .getCurrentUser()
+                .observe(this.getViewLifecycleOwner(), user -> this.viewModel.setCurrentUser(user));
+
+        this.requestAdapter.setAcceptButtonListener((adapter, item) -> this.viewModel.onAcceptClicked(item));
+        this.requestAdapter.setDenyButtonListener((adapter, item) -> this.viewModel.onDenyClicked(item));
+
+        this.followingAdapter.setUnfollowButtonListener((adapter, item) -> this.viewModel.onUnfollowClicked(item));
+
+        this.viewModel.getRequestCards()
+                .observe(this.getViewLifecycleOwner(), cards -> this.requestAdapter.setCards(cards));
+        this.viewModel.getFollowingCards()
+                .observe(this.getViewLifecycleOwner(), cards -> this.followingAdapter.setCards(cards));
+
+        this.binding.pendingRequestList.setLayoutManager(new LinearLayoutManager(this.requireContext()));
+        this.binding.pendingRequestList.setAdapter(this.requestAdapter);
+
+        this.binding.followerList.setLayoutManager(new LinearLayoutManager(this.requireContext()));
+        this.binding.followerList.setAdapter(this.followingAdapter);
 
         return this.binding.getRoot();
     }
