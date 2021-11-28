145b1af3ecb6db8977dc5c581c6d01e9d254c4fd
Wigandt de Klerk <wigandt@ualberta.ca>
2021-11-28 03:11:30 -0700

    Added new ways to get date and year in the way the app displays it for proper testing.

--- src/app/src/androidTest/java/ca/cmput301f21t22/nabu/TestResources/DateProvider.java
+++ src/app/src/androidTest/java/ca/cmput301f21t22/nabu/TestResources/DateProvider.java
@@ -11,31 +11,96 @@ public class DateProvider {
      *  A string representation of the current day
      */
     public String getCurrentDay(){
-        Calendar calendar = Calendar.getInstance();
-        int dayOfWeek = Calendar.DAY_OF_WEEK;
+        int dayOfWeek = Calendar.DAY_OF_WEEK + 1;
         String day;
         if (dayOfWeek == Calendar.SUNDAY) {
             day = "Sun";
+            day = "Mon";
         }
         else if (dayOfWeek == Calendar.MONDAY) {
             day = "Mon";
+            day = "Tue";
         }
         else if (dayOfWeek == Calendar.TUESDAY) {
             day = "Tue";
+            day = "Wed";
         }
         else if (dayOfWeek == Calendar.WEDNESDAY) {
             day = "Wed";
+            day = "Thu";
         }
         else if (dayOfWeek == Calendar.THURSDAY) {
             day = "Thu";
+            day = "Fri";
         }
         else if (dayOfWeek == Calendar.FRIDAY) {
             day = "Fri";
+            day = "Sat";
         }
         else {
             day = "Sat";
+            day = "Sun";
         }
         return day;
 
+    }
+
+    /**
+     * Returns a string representation of the current year
+     * @return
+     *  A string representation of the current year
+     */
+    public String getCurrentYear(){
+        return String.valueOf(Calendar.YEAR);
+    }
+
+    /**
+     * Constructs a string representation of the current date
+     * @return
+     *  A string representation of the current date
+     */
+    public String getCurrentDate() {
+        StringBuilder dateString = new StringBuilder();
+
+        if (Calendar.MONTH == 0) {
+            dateString.append("Jan ");
+        }
+        else if (Calendar.MONTH == 1) {
+            dateString.append("Feb ");
+        }
+        else if (Calendar.MONTH == 2) {
+            dateString.append("Mar ");
+        }
+        else if (Calendar.MONTH == 3) {
+            dateString.append("Apr ");
+        }
+        else if (Calendar.MONTH == 4) {
+            dateString.append("May ");
+        }
+        else if (Calendar.MONTH == 5) {
+            dateString.append("Jun ");
+        }
+        else if (Calendar.MONTH == 6) {
+            dateString.append("Jul ");
+        }
+        else if (Calendar.MONTH == 7) {
+            dateString.append("Aug ");
+        }
+        else if (Calendar.MONTH == 8) {
+            dateString.append("Sep ");
+        }
+        else if (Calendar.MONTH == 9) {
+            dateString.append("Oct ");
+        }
+        else if (Calendar.MONTH == 10) {
+            dateString.append("Nov ");
+        }
+        else {
+            dateString.append("Dec ");
+        }
+        dateString.append(Calendar.DAY_OF_MONTH);
+        dateString.append(", ");
+        dateString.append(Calendar.YEAR);
+        return dateString.toString();
     }
 }
